e62dba166a4b95a8b123b7a3c778488ef5e150e1
ExtremeH4wk <1435962448@qq.com>
2020-08-06 22:52:21 +0800

    更新整合首页轮播图接口

--- src/main/java/com/example/bootmybatis/BootmybatisApplication.java
+++ src/main/java/com/example/bootmybatis/BootmybatisApplication.java
@@ -2,7 +2,7 @@ package com.example.bootmybatis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
-
+//http://127.0.0.1:8888/swagger-ui.html#/
 @SpringBootApplication
 @EnableAsync
 public class BootmybatisApplication {
